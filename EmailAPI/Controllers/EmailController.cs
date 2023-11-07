d37fc7df28dd1064e6a5bada5bb752e64dcfdb4a
Rafael Casado <rafael.fernandes.newmobile@caixaconsorcio.com.br>
2023-11-07 12:59:30 -0300

    Subindo aplicação no GIT

---
+++ EmailAPI/Controllers/EmailController.cs
@@ -0,0 +1,34 @@
+﻿using EmailAPI.Domain.DTO;
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace EmailAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        protected readonly IConfiguration _configuration;
+
+        public EmailController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpPost(Name = "SendEmail")]
+        public async void Post([FromBody] Email email)
+        {
+            var apiKey = _configuration.GetSection("SendGrid")["key"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(_configuration.GetSection("SendGrid")["senderEmail"], _configuration.GetSection("SendGrid")["senderName"]);
+            var subject = email.Subject;
+            var to = new EmailAddress(email.RecipientEmail, email.RecipientName);
+            var htmlContent = email.HtmlBody;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, string.Empty, htmlContent);
+            
+            await client.SendEmailAsync(msg);
+        }
+    }
+}
---
+++ EmailAPI/Domain/DTO/Email.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailAPI.Domain.DTO
+{
+    public class Email
+    {
+        public string RecipientEmail { get; set; }
+        public string RecipientName { get; set; }
+        public string Subject { get; set; }
+        public string HtmlBody { get; set; }        
+    }
+}
